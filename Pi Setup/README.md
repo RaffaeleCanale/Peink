3889b42535bb45b37b135d65aa69cc093a383dc5
Canale <raffaelecanale@gmail.com>
2019-11-28 16:42:19 +0100

    Initial test

---
+++ Pi Setup/README.md
@@ -0,0 +1,11 @@
+## 1. Flash a Raspbian image
+
+- Download a Raspbian image (Lite is good enough): https://www.raspberrypi.org/downloads/raspbian/
+- Follow the instructions there to flash the image
+
+## 2. Enable ssh
+
+By default, ssh is disabled.
+
+- Connect to the Pi and login. User is "pi" and password is "raspberry"
+- Run `sudo raspi-config` and enable ssh in the networking options
---
+++ src/epaper/test.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+import sys
+import os
+
+import logging
+from waveshare_epd import epd7in5
+import time
+from PIL import Image,ImageDraw,ImageFont
+import traceback
+
+logging.basicConfig(level=logging.DEBUG)
+
+try:
+    logging.info("epd7in5 Demo")
+
+    epd = epd7in5.EPD()
+    logging.info("init and Clear")
+    epd.init()
+    epd.Clear()
+
+    logging.info("FOOBAR")
+    Himage = Image.open('./7in5.bmp')
+    epd.display(epd.getbuffer(Himage))
+
+    # logging.info("Clear...")
+    # epd.init()
+    # epd.Clear()
+
+    logging.info("Goto Sleep...")
+    epd.sleep()
+
+except IOError as e:
+    logging.info(e)
+
+except KeyboardInterrupt:
+    logging.info("ctrl + c:")
+    epd7in5.epdconfig.module_exit()
+    exit()
